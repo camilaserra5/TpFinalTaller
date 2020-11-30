6a6e5936a1c8cf3f2601991dbbbf28df6789aeb1
agustina <asegura@fi.uba.ar>
2020-11-30 19:02:02 -0300

    prueba de editor

--- editor/src/editor_main.cpp
+++ editor/src/editor_main.cpp
@@ -5,7 +5,21 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    QLabel label("El mapa a editar es\n");
-    label.show();
+    QWidget ventana;
+    //
+
+    QHBoxLayout configuracion(&ventana);
+    QVBoxLayout editar(&configuracion);
+    // Creo los widgets
+    QLabel label("configuracion");
+    QPushButton colores("editar colores");
+    QPushButton personajes("editar personajes");
+    // Agrego los widgets al layout vertical
+    editar.addWidget(&label);
+    editar.addWidget(&colores);
+    editar.addWidget(&personajes);
+    configuracion.addWidget(editar);
+  //  QLabel label("El mapa a editar es\n");
+    ventana.show();
     return app.exec();
 }
