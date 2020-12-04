9da76c16fc54dcb4d2fa953a4c7641581d22de20
sol <msfontenla@fi.uba.ar>
2020-12-04 19:15:14 -0300

    agrego las clases comando y movimiento

---
+++ common_src/include/comando.h
@@ -0,0 +1,18 @@
+#ifndef COMANDO_H
+#define COMANDO_H
+
+class Comando{
+  public:
+  Comando(){}
+  virtual ~Comando(){}
+  //virtual void estrategia() = 0;
+};
+
+/* comandos que se me ocurren:
+-moviemiento
+-disparo
+-agarrar objetos del piso
+-
+*/
+
+#endif
---
+++ common_src/include/movimiento.h
@@ -0,0 +1,12 @@
+#ifndef MOVIMENTO_H
+#define MOVIMIENTO_H
+#include "comando.h"
+
+class Movimiento: public Comando{
+  private:
+    int tecla_asociada = 1;
+  public:
+    Movimiento() {}
+    //void estrategia() override;
+};
+#endif
