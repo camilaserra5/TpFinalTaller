62e5241bcd069bf7a6aed26c5e9fbc83237cadb9
agustina <asegura@fi.uba.ar>
2020-12-07 14:23:13 -0300

    agrego el abrir una ventana en el servidor

--- server_src/src/servidor.cpp
+++ server_src/src/servidor.cpp
@@ -22,6 +22,19 @@ void procesar_comandos(ProtectedQueue& cola_comandos){
     }
   }
 }
+void Servidor::abrir_mapa(){
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *ventana = SDL_CreateWindow("LA VENTANITA",
+                                            SDL_WINDOWPOS_CENTERED,
+                                            SDL_WINDOWPOS_CENTERED, 600, 400,
+                                            SDL_WINDOW_SHOWN);
+    SDL_Renderer *unRender = SDL_CreateRenderer(ventana,-1,0);
+
+    SDL_SetRenderDrawColor(unRender, 0 , 255, 0, 255);
+    SDL_RenderClear(unRender);
+    SDL_RenderPresent(unRender);
+    return 0;
+}
 
 void Servidor::run(){
     bool termine = false;
