ec4495acdb4aec3b6e74c5b0db73ebca795a12c3
Stephanieizquierdo <sizquierdo>
2020-12-07 18:37:24 -0300

    agrego textura inferior

--- LogJuegoVentana/include/juego.h
+++ LogJuegoVentana/include/juego.h
@@ -1,12 +1,14 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 class Juego{
     private:
         bool corriendo;
         SDL_Window *ventana;
         SDL_Renderer *render;
+        SDL_Texture *texturaInferior;
     public:
         Juego(){}
         ~Juego(){}
