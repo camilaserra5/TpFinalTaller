8ed90ada0c5b0ba40344349c5544a7c7c1e50890
Stephanieizquierdo <sizquierdo>
2020-12-15 19:09:28 -0300

    agrego loop y ventana del juego a raycast

--- raycasting_test/raycast.cpp
+++ raycasting_test/raycast.cpp
@@ -50,6 +50,18 @@ int main() {
           }*/
         rayo.aumentarAnguloBarrido();
     }
+    Juego *juego = new Juego();
+    try {
+        juego->inicializar("Wolfstein", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+        while (juego->estaCorriendo()) {
+            juego->handleEvents();
+            juego->actualizar();
+            juego->renderizar();
+        }
+        juego->clean();
+    } catch (...) {
+        std::cout << "error";
+    }
     return 0;
 
 }
