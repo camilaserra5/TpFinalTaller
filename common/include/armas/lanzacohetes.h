56231b473362f8f3ad7d7f58073fbf5e71da4b31
agustina <asegura@fi.uba.ar>
2021-01-18 17:47:19 -0300

    agrego lanzacohetes

---
+++ common/include/armas/lanzacohetes.h
@@ -0,0 +1,20 @@
+#ifndef _LANZA_COHETES_H
+#define _LANZA_COHETES_H
+
+#define DISTANCIA_MAX 2000000
+
+#include "arma.h"
+#include "items/item.h"
+
+class LanzaCohetes: public Arma, public Item{
+    public:
+        LanzaCohetes(Posicion &posicion) : Arma(DISTANCIA_MAX, 1),Item (posicion) {}
+
+        ~LanzaCohetes() {}
+
+        void atacar(int distancia_a_pared, Jugador *jugador, std::map<int, Jugador *> &jugadores) override{}
+
+        void obtenerBeneficio(Jugador* jugador) override{}
+};
+
+#endif
