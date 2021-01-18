3127dec23e1feac26631d2aae29e7721d124e8d4
Cami Serra <camilaserra5@gmail.com>
2021-01-18 18:20:27 -0300

    change client_src to client

--- raycasting_test/juego.cpp
+++ raycasting_test/juego.cpp
@@ -25,8 +25,8 @@ void Juego::inicializar(const char *titulo, int xpos, int ypos, int ancho, int a
     } else {
         this->corriendo = false;
     }
-    this->texturaInferior = new Textura("../../client_src/resources/images/ParteInferior.png", this->render);
-    ObjetoJuego* enemigo = new ObjetoJuego("../../client_src/resources/images/Guard.png", this->render, /*50, 50,*/0,0,78,78/*100,100*/);//poner constantes
+    this->texturaInferior = new Textura("../../client/resources/images/ParteInferior.png", this->render);
+    ObjetoJuego* enemigo = new ObjetoJuego("../../client/resources/images/Guard.png", this->render, /*50, 50,*/0,0,78,78/*100,100*/);//poner constantes
     this->objetos.push_back(enemigo);
 }
 
