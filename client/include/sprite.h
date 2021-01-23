b3832322dd0690c75ada90216066765f42b2a436
agustina <asegura@fi.uba.ar>
2021-01-23 16:30:22 -0300

    agrego clase sprite, no anda. help stefy

---
+++ client/include/sprite.h
@@ -0,0 +1,30 @@
+#ifndef _SPRITE_H
+#define _SPRITE_H
+
+
+#include "textura.h"
+#include ""
+
+/*
+class Sprite{
+    private:
+        Textura* textura;
+        std::vector<SDL_Rect> frames
+
+    public:
+
+        Sprite(SDL_Renderer* render, const char* rutaimg, int frames, int frames_a, int frames_l,
+                int ancho largo){
+                SDL_Rect rect = {0, 0, frame_h, frame_w};
+                for (int i = 0; i < frames; i++) {
+                    this->frames.push_back(rect);
+                    rect.x += frame_a;
+
+          }
+        Sprite(SDL_Renderer* render, int ancho, int largo, const char* rutaimg,
+              int cantFrames, int velocidad);
+        ~Sprite();
+        void renderizar(int x, int y, int angulo);
+
+}
+*/
