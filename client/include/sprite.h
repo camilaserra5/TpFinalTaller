770cb89b9a348f48383c230d5c630ede92d52ee3
stephanieizquierdo <sizquierdo@fi.uba.ar>
2021-01-27 14:55:23 -0500

    agrego cpps

--- client/include/sprite.h
+++ client/include/sprite.h
@@ -6,7 +6,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-
 class Sprite{
     private:
         Textura* textura;
@@ -14,24 +13,9 @@ class Sprite{
 
     public:
 
-        Sprite(SDL_Renderer* render, const char* rutaimg, int x, int y, int h, int w){
-
-            this->infoSprite.x = x* w;
-            this->infoSprite.y = y* h;
-            this->infoSprite.w = w;
-            this->infoSprite.h = h;
-            this->textura = new Textura(rutaimg,render);
-
-        }
+        Sprite(SDL_Renderer* render, const char* rutaimg, int x, int y, int h, int w);
         ~Sprite(){}
-        void renderizar(int x, int y, int angulo){
-            SDL_Rect destino;
-            destino.x = x;
-            destino.y = y;
-            destino.h = this->infoSprite.h;
-            destino.w = this->infoSprite.w;
-            this->textura->renderizar(&this->infoSprite, destino);
-        }
+        void renderizar(int x, int y, int angulo);
 
 };
 
--- client/src/client_main.cpp
+++ client/src/client_main.cpp
@@ -1,10 +1,10 @@
-#include "../include/client.h"
+#include "../include/cliente.h"
 
 #define OK_CODE 0
 #define ERR_CODE 1
 
 int main(int argc, char *argv[]) {
-    Client client;
+    Cliente client(reinterpret_cast<const char *>(1), reinterpret_cast<const char *>(2));
     client.run();
     return OK_CODE;
 }
