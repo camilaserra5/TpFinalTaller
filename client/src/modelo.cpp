8d650e5d3c94ef784ba678e89482746c5096cda5
stephanieizquierdo <sizquierdo@fi.uba.ar>
2021-01-27 17:47:32 -0500

    agrego cosas pero estan comentadas

--- client/src/modelo.cpp
+++ client/src/modelo.cpp
@@ -2,24 +2,23 @@
 
 
 Modelo::Modelo(Ventana* ventana, int idJugador):
-    ventana(ventana),
-    idJugador(idJugador),
-    jugadores(),
-    entidades(){}
+        ventana(ventana),
+        idJugador(idJugador),
+        jugadores(),
+        entidades(){}
 
 Modelo::~Modelo(){}
 
 void Modelo::inicializar(){
-      Player* player = new Player("../../client/resources/images/Weapons.png", this->ventana->obtener_render(), this->idJugador);
-      this->jugadores.insert(std::make_pair(this->idJugador, player));
-}
-
-void Modelo::actualizarJugador(int x, int y, int vida, int angulo, int id, int idArma){
-    this->jugadores[id]->settear_estado(x,y,vida,angulo, idArma);
+    Player* player = new Player("../../client/resources/images/Weapons.png", this->ventana->obtener_render(), this->idJugador);
+    this->jugadores.insert(std::make_pair(this->idJugador, player));
 }
 
 void Modelo::renderizar(){
-      this->jugadores[this->idJugador]->renderizar();
+    this->jugadores[this->idJugador]->settear_estado(200,200, 100, 50);
+    this->jugadores[this->idJugador]->renderizar();
 }
 
-
+void Modelo::actualizarJugador(int x, int y, int vida, int angulo, int id){
+    this->jugadores[id]->settear_estado(x,y,vida,angulo/*tipo de arma*/);
+}
