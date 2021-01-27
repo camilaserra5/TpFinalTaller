fa24934752b321e070e8e584bee74e1304df3e74
agustina <asegura@fi.uba.ar>
2021-01-27 18:16:34 -0300

    fix de info jugador

--- client/include/informacionJugador.h
+++ client/include/informacionJugador.h
@@ -1,30 +1,44 @@
 #ifndef _INFORMACION_JUGADOR_H
 #define _INFORMACION_JUGADOR_H
 
-#include "ventana.h"
-#include "texto.h"
+
+#include "label.h"
+#include "fonts.h"
 
 class InfoJugador{
 public:
 
-      InfoJugador(Ventana& ventana, int vida, int nivel, int puntaje){
-          this->vida = Texto(ventana, vida);
-          this->nivel = Texto(ventana, nivel);
-          this->puntaje = Texto(ventana, puntaje);
+      InfoJugador(SDL_Renderer* render, int vida, int nivel, int puntaje){
+          Fonts fuentes;
+          fuentes.addFont("info", "../resources/fonts/wolfenstein.ttf", 60);
+          SDL_Color blanco = {255, 255, 255, 255};
+          std::string vidainfo = std::to_string(vida);
+          std::string nivelinfo = std::to_string(nivel);
+          std::string puntajeinfo = std::to_string(puntaje);
+          //Label label(500, 0, vidainfo, fuentes.getFont("info"), blanco, render);
+          this->vida = new Label(200, 200, "PEPEEE", fuentes.getFont("info"), blanco, render);
+          this->vida->setLabelText();
+          //this->nivel = Texto(ventana, nivel);
+          //this->puntaje = Texto(ventana, puntaje);
       }
-      ~InfoJugador();
+      ~InfoJugador(){}
+      void actualizarDatosJugador(int vida, int nivel, int puntaje){
+          std::string vidainfo = std::to_string(vida);
+          std::string nivelinfo = std::to_string(nivel);
+          std::string puntajeinfo = std::to_string(puntaje);
+          //this->vida->actualizarTexto(vidainfo);
+      }
       void renderizar(){
-        /*
-          this->vida.renderizar();
-          this->nivel.renderizar();
-          his->puntaje.renderizar();
-          */
+          this->vida->draw();
+          //this->nivel.renderizar();
+          //his->puntaje.renderizar();
+
       }
 
 private:
-        Texto vida;
-        Texto nivel;
-        Texto puntaje;
+        Label* vida;
+        //Label nivel;
+        //Label puntaje;
 
 
 };
