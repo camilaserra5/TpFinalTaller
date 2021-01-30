bc41a238b37214d735534956bcdb5995dd10ea2f
agustina <asegura@fi.uba.ar>
2021-01-30 18:58:59 -0300

    agrego clas enemigo, esta comentada

--- client/include/enemigo.h
+++ client/include/enemigo.h
@@ -1,16 +1,25 @@
 #ifndef _ENEMIGO_H
 #define _ENEMIGO_H
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "animacion.h"
+#include <map>
+#include <string>
+
 class Enemigo{
 public:
-      Enemigo(SDL_Render* render, int id);
+      Enemigo(SDL_Renderer* render, int idArmaJugador);
       ~Enemigo();
       void actualizar(int posx, int posy);
       void renderizar();
 private:
-      int id;
+      int idArma;
       int posx;
       int posy;
+      int angulo;
+      int anguloJugador;
+      std::multimap<int, std::pair<std::string, Animacion>> enemigos;
 };
 
 #endif
