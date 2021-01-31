7ed8917afca5cfd0f21c6841479f4052fe9737ee
sol <msfontenla@fi.uba.ar>
2021-01-31 14:48:11 -0300

    agrego el .cpp de animacion

--- client/include/animacion.h
+++ client/include/animacion.h
@@ -25,38 +25,10 @@ class Animacion{
                                int frames,
                                int frame_h,
                                int frame_w,
-                              int fila){
-
-          SDL_Rect rect = {0, fila*frame_h, frame_h, frame_w};
-          for (int i = 0; i < frames; i++) {
-              this->frames.push_back(rect);
-              rect.x += frame_w;
-          }
-          this->textura = new Textura(rutaimg, render);
-          this->frame_h = frame_h;
-          this->frame_w = frame_w;
-          this->frameActual = 0;
-          this->velocidad = 1000;
-          this->tiempoViejo = 0;
-       }
+                              int fila);
         ~Animacion(){}
 
-    bool  renderizar(int posx, int posy){
-          bool termine = false;
-          if (tiempoViejo + velocidad > SDL_GetTicks()){
-              termine = true;
-          }
-          tiempoViejo = SDL_GetTicks();
-
-        if (this->frameActual >= frames.size()){
-              this->frameActual = 0;
-          }
-          SDL_Rect r = {posx, posy, this->frame_h*2, this->frame_w*2}; // Donde se renderiza
-          textura->renderizar(&frames[this->frameActual], r);
-          this->frameActual ++;
-          SDL_Delay(100);
-          return termine;
-    }
+    bool  renderizar(int posx, int posy);
 };
 
 #endif
