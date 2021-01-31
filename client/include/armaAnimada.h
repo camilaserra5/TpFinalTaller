06df809484e946d4fe5b25f9322ff766307840fe
sol <msfontenla@fi.uba.ar>
2021-01-31 16:54:08 -0300

    arreglo merge error

--- client/include/armaAnimada.h
+++ client/include/armaAnimada.h
@@ -11,11 +11,7 @@ class ArmaAnimada{
     public:
           ArmaAnimada(SDL_Renderer* render);
           ~ArmaAnimada();
-<<<<<<< HEAD
-          void renderizar(bool& disparando);
-=======
           void renderizar(bool disparando);
->>>>>>> 4269baa58c62fc635ff9f9b799dfc21193b8979b
           void actualizar(int idArma);
 
     private:
