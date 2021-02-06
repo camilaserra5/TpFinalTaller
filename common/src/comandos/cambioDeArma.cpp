fb87d778b3c294efa40686ce9a8dc210b0e71337
sol <msfontenla@fi.uba.ar>
2021-02-06 10:14:04 -0300

    agrego serializacion y deserializacion de cambio de arma

--- common/src/comandos/cambioDeArma.cpp
+++ common/src/comandos/cambioDeArma.cpp
@@ -5,8 +5,18 @@ void CambioDeArma::ejecutar(EstadoJuego &estadoJuego){
 }
 
 std::vector<char> CambioDeArma::serializar(){
-  std::vector<char> info;
-  return info;
+  std::vector<char> informacion;
+  std::vector<char> aux(4);
+  aux = numberToCharArray(idJugador);
+  informacion.insert(informacion.end(), aux.begin(), aux.end());
+  aux = numberToCharArray(static_cast<int>(Accion::cambioDeArma));
+  informacion.insert(informacion.end(), aux.begin(), aux.end());
+  return informacion;
 }
 
-void CambioDeArma::deserializar(std::vector<char> &serializado){}
+void CambioDeArma::deserializar(std::vector<char> &serializado){
+  std::vector<char> sub(4);
+  int idx = 0;
+  sub = std::vector<char>(&serializado[idx], &serializado[idx + 4]);
+  this->idJugador = charArrayToNumber(sub);
+}
