e0cfebfff0f7fad0aa574576bf9e4f2084f7ab18
stephanieizquierdo <sizquierdo@fi.uba.ar>
2021-02-10 18:01:32 -0500

    aniado funciones de sondeo de enemigos y avance hacia el(todavia no implemanta, esta con comentarios)

--- client/src/LUA/modulo.lua
+++ client/src/LUA/modulo.lua
@@ -27,10 +27,44 @@ function proximo_movimiento(pos_x, pos_y)
     end
 end
 
+--Revisa en un rango si hay enemigos en el mapa  PREGUNTA: EL MAPA LO TIENE QUE RECIBIR LAS FUNCIONES O PUEDE AGARRARLA COMO ALGO TIPO EXTERN?
+--los jugadores aparecen con numeros mayores o iguales a 100
+function posicion_enemigo_cerca(pos_x, pos_y)
+    for i = pos_x - 10, i = 10, 1 do
+        for j = pos_y - 10, j + 10, 1 do
+            if (mapa[i][j] >= 100) then
+                return {posicionX = i, posicionY = j}--true --puede devolver tambien la posicion?
+            end
+        end
+    end
+    return nil--false
+end
+
+-- Teniendo ya la posicion del enemigo, el jugador lua se tiene que acercar a el
+-- siempre en posiciones validas
+function avanzar_hacia_enemigo(posicion_enemigo, pos_x, pos_y)
+    if (posicion_enemigo.posicionX > pos_x) then
+        if (posicion_enemigo.posicionY > pos_y) then
+            --avanzar hacia adelante y a la derecha, pero tengo que chequear que pueda avanzar porque se tiene que mover
+            -- siempre en posiciones validas
+        else
+            -- avanzo a la derecha y hacia atras
+            --CHEQUEO SI PUEDO
+    else
+        if (posicion_enemigo.posicionY > pos_y) then
+            --avanzar hacia adelante y a la izquierda, pero tengo que chequear que pueda avanzar porque se tiene que mover
+            -- siempre en posiciones validas
+        else
+            -- avanzo a la izquierda y hacia atras
+            --CHEQUEO SI PUEDO
+
+end
+
 function moverse(pos_x,pos_y)
-    local siguiente_mov = proximo_movimiento(pos_x,pos_x)
-    if(enemigo_cerca()) then
-        return avanzar_hacia_enemigo()
+    local siguiente_mov = proximo_movimiento(pos_x, pos_y)
+    local posicion_enemigo = posicion_enemigo_cerca(pos_x, pos_y)
+    if (posicion_enemigo) then
+        return avanzar_hacia_enemigo(posicion_enemigo, pos_x, pos_y)
     end
     return siguiente_mov
 end
