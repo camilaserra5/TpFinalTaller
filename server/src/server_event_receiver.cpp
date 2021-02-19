33e8bc11a0ab326f50253218bccc8ca261ae3220
agustina <asegura@fi.uba.ar>
2021-02-19 12:00:48 -0300

    agrego try catchs para sockets

--- server/src/server_event_receiver.cpp
+++ server/src/server_event_receiver.cpp
@@ -1,6 +1,6 @@
 #include "../include/server_event_receiver.h"
 #include <vector>
-
+#include "socket_error.h"
 
 void Server_Event_Receiver::run() {
     while (this->corriendo) {
@@ -8,9 +8,12 @@ void Server_Event_Receiver::run() {
             std::vector<char> informacion = this->protocolo->recibir();
             Comando *comando = protocolo->deserializarComando(informacion);
             this->comandos.aniadir_dato(comando);
+        }catch (const SocketError& exc){
+            std::cout << exc.what() << std::endl;
+            this->cerrar();
         } catch (std::exception &exc) {
             std::cout << exc.what() << std::endl;
-            this->corriendo = false;
+            this->cerrar();
         }
     }
 }
