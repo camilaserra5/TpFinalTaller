3ae384dcd5114bf39cf34ae6dbb3d644c4b237b5
agustina <asegura@fi.uba.ar>
2021-02-21 18:02:02 -0300

    fix en salir del aceptador

--- common/include/blocking_queue.h
+++ common/include/blocking_queue.h
@@ -6,6 +6,8 @@
 #include <exception>
 #include <iostream>
 #include <condition_variable>
+#include "protected_queue.h"
+#include <atomic>
 
 template<class T>
 class BlockingQueue {
@@ -13,9 +15,10 @@ private:
     std::queue<T> cola_datos;
     std::mutex m;
     std::condition_variable esta_vacia;
+    std::atomic<bool> esta_cerrado;
 
 public:
-    BlockingQueue() {}
+    BlockingQueue(): esta_cerrado(false) {}
 
     ~BlockingQueue() {}
 
@@ -27,12 +30,19 @@ public:
 
     T pop() {
         std::unique_lock<std::mutex> l(this->m);
-        while (cola_datos.empty()) {
+        while (cola_datos.empty() && !esta_cerrado) {
             esta_vacia.wait(l);
+        }
+        if (esta_cerrado){
+              throw QueueException("no hay elementos en la cola\n");
         }
         T objeto = this->cola_datos.front();
         this->cola_datos.pop();
         return objeto;
+    }
+    void cerrar(){
+        this->esta_cerrado = true;
+        esta_vacia.notify_all();
     }
 
 };
