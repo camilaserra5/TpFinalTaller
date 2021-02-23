302c59952adfe3fc11917ee870de1b4a48a84326
agustina <asegura@fi.uba.ar>
2021-02-23 17:28:38 -0300

    maas fix

--- client/src/informacionJugador.cpp
+++ client/src/informacionJugador.cpp
@@ -7,7 +7,7 @@
 #define RUTA_SONIDO_CANION SOUNDS_DIR MUERTE
 
 InfoJugador::InfoJugador(SDL_Renderer *render, int vida, int nivel, int puntaje,
-                         int cantVidas, int balas) : musica(MUERTE) {
+                         int cantVidas, int balas){
     Fonts fuentes;
     fuentes.addFont("info", FONT, 35);
     SDL_Color blanco = {255, 255, 255, 255};
@@ -22,7 +22,6 @@ InfoJugador::InfoJugador(SDL_Renderer *render, int vida, int nivel, int puntaje,
     this->cantVidas = new Label(270, 570, cantVidasinfo, fuentes.getFont("info"), blanco, render);
     this->balas = new Label(550, 570, balasinfo, fuentes.getFont("info"), blanco, render);
     this->texturaInferior = new Textura(LOWER_TEXTURE_ROOT, render);
-    this->murio = false;
 }
 
 InfoJugador::~InfoJugador() {
@@ -35,9 +34,6 @@ InfoJugador::~InfoJugador() {
 
 void InfoJugador::actualizarDatosJugador(int vida, int nivel, int puntaje, int cantVidas,
                                          int balas) {
-    if (cantVidas < std::stoi(this->cantVidas->getTexto().c_str())){
-        this->murio = true;
-    }
     std::string vidainfo = std::to_string(vida);
     std::string nivelinfo = std::to_string(nivel);
     std::string puntajeinfo = std::to_string(puntaje);
@@ -63,9 +59,6 @@ void InfoJugador::renderizar() {
     this->puntaje->draw();
     this->cantVidas->draw();
     this->balas->draw();
-    if (this->murio){
-        this->musica.play(2);
-    }
 }
 
 int InfoJugador::getPuntaje() {
